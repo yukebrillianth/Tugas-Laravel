eae3dccf2ab120717b4044534e92cce62b6c6cde
yuke brilliant <yukebrilliant@gmail.com>
2020-06-30 17:47:20 +0700

    ngetest push dari vscode

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HTML Challange</title>
+    <title>HTML Challange | Laravel</title>
 </head>
 
 <body>
